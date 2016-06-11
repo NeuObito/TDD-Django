cf7c25b8cdd61ac94b2175d09ce94ded8ae7bd64
Nbito <Nbito>
2016-06-11 15:38:28 +0800

    Refactor gine page view to user a template

--- lists/views.py
+++ lists/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.http import HttpRequest
 # Create your views here.
 # You can write view here
 
 
 def home_page(request):
-    return HttpResponse('<html><title>To-Do lists</title></html>')
+    return render(request, 'home.html')
+
+
+def test_home_page_returns_correct_html(self):
+    request = HttpRequest()
+    response = home_page(request)
+    expected_html = render_to_string('home.html')
+    self.assertEqual(response.content.decode(), expected_html)
